e272ef6b943f05359ea669615824295f6ffdc33c
hp <Shivraj.Jadhav@external.hella.com>
2019-04-16 23:57:02 +0530

    Commiting source code

---
+++ app/src/main/java/com/example/bleapp/Constants.java
@@ -0,0 +1,12 @@
+package com.example.bleapp;
+
+
+public class Constants {
+
+    public static final int REQUEST_BLUETOOTH_ENABLE_CODE = 101;
+    public static final int REQUEST_LOCATION_ENABLE_CODE = 101;
+
+    public static final int SCAN_PERIOD = 10000;
+
+
+}
